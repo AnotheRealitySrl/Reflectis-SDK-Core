de17da8d00b0a660bfb83d63b03d8bf95414d2f0
Emanuele Torelli <emanueletorelli@outlook.com>
2022-04-01 18:57:43 +0200

    Restore Logic folder from SPACS Core

---
+++ Runtime/Logic/Smoothers/PoseSmoother.cs
@@ -0,0 +1,19 @@
+﻿using SPACS.Utilities;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace SPACS.Logic.Smoothers
+{
+    ///////////////////////////////////////////////////////////////////////////
+    /// <summary>
+    /// Processes pose values in order to smooth them.
+    /// </summary>
+    public class PoseSmoother : AbstractSmoother<Pose>
+    {
+        ///////////////////////////////////////////////////////////////////////////
+        /// <inheritdoc/>
+        protected override Pose Average(IEnumerable<Pose> values) => values.Average();
+    }
+}
---
+++ Runtime/Logic/Transformers/FloatToFloat.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+#if !UNITY_2020_1_OR_NEWER
+using static SPACS.Logic.Events.BasicEvents;
+#endif
+
+namespace SPACS.Logic.Transformers
+{
+    ///////////////////////////////////////////////////////////////////////////
+    /// <summary>
+    /// This transformer applies a multiplier and an offset to a float value
+    /// and fires the transformed value into an event
+    /// </summary>
+    public class FloatToFloat : MonoBehaviour
+    {
+        [Tooltip("The multiplier to apply on the input float")]
+        [SerializeField]
+        private float multiplier = 1.0f;
+
+        [Tooltip("The offset to apply on the input float")]
+        [SerializeField]
+        private float offset = 0.0f;
+
+        [Tooltip("The event fired when the transformed value has been calculated")]
+        [SerializeField]
+#if UNITY_2020_1_OR_NEWER
+        private UnityEvent<float> OnProcess = default;
+#else
+        private FloatUnityEvent OnProcess = default;
+#endif
+
+        ///////////////////////////////////////////////////////////////////////////
+        /// <summary>
+        /// Call this method to provide the input value to this transformer. The 
+        /// input value will be processed and the resulting value will be fired
+        /// with the OnProcess event
+        /// </summary>
+        /// <param name="f">The input value</param>
+        public void Process(float f)
+        {
+            if (!gameObject.activeInHierarchy)
+                return;
+
+            float result = (offset + f) * multiplier;
+            OnProcess.Invoke(result);
+        }
+    }
+}
