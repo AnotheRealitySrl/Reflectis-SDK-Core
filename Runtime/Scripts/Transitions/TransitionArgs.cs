a7fff4c1cca33832ee8cdf8e03dbf77ac88bf49a
Emanuele Torelli <emanueletorelli@outlook.com>
2022-04-07 18:42:33 +0200

    Restore logic folder, remove transitions logic

--- Runtime/Scripts/Transitions/TransitionArgs.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SPACS.Core.Transitions
-{
-    public class TransitionArgs
-    {
-        //do not animate Enter/Exit transition (overrides TransitionAnimator configuration)
-        public bool skipTransitionAnimation = false;
-    }
-}
