0436da2b8d1024c77028628a09e86f03ece4ee59
Emanuele Torelli <emanueletorelli@outlook.com>
2022-07-01 19:43:51 +0200

    Add Vector3 deserialization util

--- Runtime/Scripts/Extensions/VectorExtensions.cs
+++ Runtime/Scripts/Extensions/VectorExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 
 using UnityEngine;
@@ -17,6 +18,24 @@ namespace SPACS.Utilities
             average /= vectors.Count();
 
             return average;
+        }
+
+        ///////////////////////////////////////////////////////////////////////////
+        public static Vector3? DeserializeVector3(this string sourceString)
+        {
+            if (sourceString == null || sourceString == string.Empty)
+            {
+                return null;
+            }
+
+            // Trim extranious parenthesis and split delimted values into an array
+            string[] splitString = sourceString.Substring(1, sourceString.Length - 2).Split(", "[0]);
+
+            // Build new Vector3 from array elements
+            return new Vector3(
+                float.Parse(splitString[0], CultureInfo.InvariantCulture), 
+                float.Parse(splitString[1], CultureInfo.InvariantCulture), 
+                float.Parse(splitString[2], CultureInfo.InvariantCulture));
         }
 
     }
