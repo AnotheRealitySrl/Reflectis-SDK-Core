64372a4b655cbb6fc152653f7d356525baa55d4b
Gabriele Castaldo <gabriele.castaldo@outlook.com>
2022-11-21 17:13:15 +0100

    Updated Interface

--- Systems/Runtime/Scripts/IComunicationSystem.cs
+++ Systems/Runtime/Scripts/IComunicationSystem.cs
@@ -7,12 +7,47 @@ namespace SPACS.SDK.Systems
 {
     public interface IComunicationSystem : ISystem
     {
+        void AskPermissions();
+        void LoadAuthenticationData(string appId, string token, string channelName);
+
+        void LoadEngine();
+
+        //Buono sia per video che audio 
         void ConnectToRoom(Room _room);
+
+        void DisconnectFromRoom();
+
+        void MuteAllRemoteStream(Room room);
+
+        void MuteUser(RemoteUser _user);
+
+        void DestroyVideoView(uint uid);
+
+        void MakeVideoView(uint uid, string parentName, GameObject image, bool islocal, string channelId = "");
+
+        string GetChannelName();
     }
 
     public struct Room
     {
         public string Name;
+
+        public bool hasAudio;
+        public bool hasVideo;
+
+        public bool muteAudio;
+        public bool muteVideo;
+
+        public int videoHeight;
+        public int videoWidth;
+    }
+
+    public struct RemoteUser
+    {
+        public uint uId;
+
+        public bool muteAudio;
+        public bool muteVideo;
     }
 }
 
