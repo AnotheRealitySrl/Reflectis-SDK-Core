dbda58bf5a439e575b0e3ce40fd6bc881b8e9725
Gabriele Castaldo <gabriele.castaldo@outlook.com>
2022-11-22 19:45:05 +0100

    Added Events in the IComunicationSyste Interface

--- Systems/Runtime/Scripts/IComunicationSystem.cs
+++ Systems/Runtime/Scripts/IComunicationSystem.cs
@@ -1,7 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using SPACS.Core;
+using System;
 
 namespace SPACS.SDK.Systems
 {
@@ -26,6 +26,27 @@ namespace SPACS.SDK.Systems
         void MakeVideoView(uint uid, string parentName, GameObject image, bool islocal, string channelId = "");
 
         string GetChannelName();
+
+        #region Actions
+
+        StringEvent OnError { get; }
+        StringEvent OnGetMessage { get; }
+
+        UnityEvent OnJoinChannelSuccess { get; }
+
+        UnityEvent OnRejoinChannelSuccess { get; }
+        UnityEvent OnLeaveChannel { get; }
+
+        UnityEvent OnUserJoined { get; }
+        UnityEvent OnUserLeft { get; }
+
+        UnityEvent OnUserOffline { get; }
+
+        UnityEvent OnRemoteAudioStateChanged { get; }
+        UnityEvent OnRemoteVideoStateChanged { get; }
+
+        UnityEvent OnClientRoleChanged { get; }
+        #endregion
     }
 
     public struct Room
@@ -48,6 +69,11 @@ namespace SPACS.SDK.Systems
 
         public bool muteAudio;
         public bool muteVideo;
+    }
+
+    [System.Serializable]
+    public class StringEvent : UnityEvent<string>
+    {
     }
 }
 
