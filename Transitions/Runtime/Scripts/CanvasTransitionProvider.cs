ecaafc9059af2583705fd00079dca311c6024164
Emanuele Torelli <emanueletorelli@outlook.com>
2022-12-22 16:16:27 +0100

    Add CanvasTransitionProvider and dependency to DOTween

---
+++ Transitions/Runtime/Scripts/CanvasTransitionProvider.cs
@@ -0,0 +1,35 @@
+using DG.Tweening;
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+namespace SPACS.SDK.Transitions
+{
+    public class CanvasTransitionProvider : AbstractTransitionProvider
+    {
+        [SerializeField] private CanvasGroup canvasGroup;
+        [SerializeField] private float fadeTime = 1f;
+        [SerializeField] private Ease easingFunction = Ease.InOutQuad;
+
+        private void Awake()
+        {
+            if (!canvasGroup)
+            {
+                canvasGroup = GetComponentInChildren<CanvasGroup>();
+            }
+        }
+
+        public override async Task EnterTransitionAsync()
+        {
+            await canvasGroup.DOFade(1f, fadeTime).SetEase(easingFunction).AsyncWaitForCompletion();
+        }
+
+        public override async Task ExitTransitionAsync()
+        {
+            await canvasGroup.DOFade(0f, fadeTime).SetEase(easingFunction).AsyncWaitForCompletion();
+        }
+    }
+}
