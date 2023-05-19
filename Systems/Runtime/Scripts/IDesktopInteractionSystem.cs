c41615475952112bf488e8f0398d3ffb9fb3a9dc
Emanuele Torelli <emanueletorelli@outlook.com>
2023-05-19 14:48:09 +0200

    Add IDesktopInteractionSystem interface

---
+++ Systems/Runtime/Scripts/IDesktopInteractionSystem.cs
@@ -0,0 +1,17 @@
+using SPACS.Core;
+
+namespace SPACS.SDK.Systems
+{
+    public interface IDesktopInteractionSystem : ISystem
+    {
+        public enum DesktopSelectionType
+        {
+            Mouse = 1,
+            Proximity = 2
+        }
+
+        void StartSelecting(BaseInteractableGO interactable, DesktopSelectionType mode);
+        void StopSelecting(BaseInteractableGO interactable, DesktopSelectionType mode);
+    }
+}
+
