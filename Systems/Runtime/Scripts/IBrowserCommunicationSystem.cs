b4d73d2b8c6e11a244aad48c8d77944e48b2dac5
Riccardo Verza <feelthecabbage@gmail.com>
2023-06-13 16:17:15 +0200

    Added new interface for new BrowserCommunication system.

---
+++ Systems/Runtime/Scripts/IBrowserCommunicationSystem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using SPACS.Core;
+
+namespace SPACS.SDK.Systems
+{
+    public interface IBrowserCommunicationSystem : ISystem
+    {
+        public event Action<string> OnWebMessageReceived;
+
+        /// <summary>
+        /// Sends a string to JS.
+        /// </summary>
+        /// <param name="payload">The payload to send.</param>
+        public void SendMessageToWeb(string payload);
+
+        /// <summary>
+        /// Sends a raw message to JS.
+        /// </summary>
+        /// <param name="payload">The payload to send.</param>
+        public void SendRawToWeb(string payload);
+    }
+}
