b8c1391447d9944a178ba80a4c19b4c843f7dc77
Emanuele Torelli <emanueletorelli@outlook.com>
2023-06-16 15:57:11 +0100

    Move documentation to special folders

---
+++ DesktopInteraction/Runtime/Scripts/IDesktopInteractionSystem.cs
@@ -0,0 +1,20 @@
+using SPACS.Core;
+
+namespace SPACS.SDK.DesktopInteraction
+{
+    public interface IDesktopInteractionSystem : ISystem
+    {
+        public enum DesktopSelectionType
+        {
+            Mouse = 1,
+            Proximity = 2
+        }
+
+        void Deinit();
+        void EnableDesktopInteraction(bool enable);
+
+        void StartSelecting(BaseInteractableGO interactable, DesktopSelectionType mode);
+        void StopSelecting(BaseInteractableGO interactable, DesktopSelectionType mode);
+    }
+}
+
