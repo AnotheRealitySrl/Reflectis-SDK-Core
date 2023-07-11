b7e30e4e80292071b30f7a651fe2055731dc6bac
Lucas Calzà <lucascalza.318@gmail.com>
2023-07-11 12:38:16 +0200

    Work on objspawner systrem

---
+++ ObjectSpawner/Runtime/Scripts/SpawnSystemInputMapper.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
+using UnityEngine.InputSystem;
+using Reflectis.SDK.Core;
+
+namespace Reflectis.SDK.ObjectSpawner
+{
+    public class SpawnSystemInputMapper : MonoBehaviour
+    {
+        protected SpawnableData spawnableData;
+
+        protected void RegisterSecondaryActionCallback(InputAction action)
+        {
+            action.started += ButtonActionCallback;
+            action.performed += ButtonActionCallback;
+            action.canceled += ButtonActionCallback;
+        }
+
+        protected void ButtonActionCallback(CallbackContext context)
+        {
+            if (context.action.enabled)
+            {
+                CallObjectSpawnerSystem(spawnableData);
+            }
+        }
+
+        protected void CallObjectSpawnerSystem(SpawnableData data)
+        {
+            SM.GetSystem<ObjectSpawnerSystem>().CheckEntireFov(data);
+        }
+
+        protected static InputAction GetInputAction(InputActionReference actionReference)
+        {
+#pragma warning disable IDE0031 // Use null propagation -- Do not use for UnityEngine.Object types
+            return actionReference != null ? actionReference.action : null;
+#pragma warning restore IDE0031
+        }
+    }
+}
