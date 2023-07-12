1ae8728efa81bc96c545fd83d96130bfa073ab15
AlejandroDVDSM <alejandro.david@risingpixel.com>
2023-07-12 10:05:08 +0100

    Implement Agora Text Chat interface

--- TextChat/Runtime/Scripts/ITextChatSystem.cs
+++ TextChat/Runtime/Scripts/ITextChatSystem.cs
@@ -1,12 +1,59 @@
 using Reflectis.SDK.Core;
 
+using AgoraChat;
+
 namespace Reflectis.SDK.TextChat
 {
     /// <summary>
     /// Manages the text communication among players.
     /// </summary>
-    public interface ITextChatSystem : ISystem
+    public interface ITextChatSystem : ISystem, IChatManagerDelegate
     {
-        
+        /// <summary>
+        /// Key of the chat project in Agora console.
+        /// Looks like: 12345678#1234567
+        /// </summary>
+        string AppKey { get; }
+
+        /// <summary>
+        /// Adds a chat manager listener
+        /// </summary>
+        void AddChatDelegate();
+
+        /// <summary>
+        /// Removes a chat manager listener
+        /// </summary>
+        void RemoveChatDelegate();
+
+        /// <summary>
+        /// Sends a new message to a specific user
+        /// </summary>
+        /// <param name="username">Name of the user who will receive the message</param>
+        /// <param name="msgContent">Content of the message</param>
+        void SendMessageToUser(string username, string msgContent);
+
+        /// <summary>
+        /// Sends a message to a specific channel
+        /// </summary>
+        /// <param name="channelId">Id of the channel that will receive the message</param>
+        /// <param name="msgContent">Content of the message</param>
+        void SendMessageToChannel(string channelId, string msgContent);
+
+        /// <summary>
+        /// Get all the public channels from the Agora server
+        /// </summary>
+        void FetchChannelsFromServer();
+
+        /// <summary>
+        /// Join a channel
+        /// </summary>
+        /// <param name="channelId">Id of the channel</param>
+        void JoinTextChannel(string channelId);
+
+        /// <summary>
+        /// Leave a channel
+        /// </summary>
+        /// <param name="channelId">Id of the channel</param>
+        void LeaveTextChannel(string channelId);
     }
 }
