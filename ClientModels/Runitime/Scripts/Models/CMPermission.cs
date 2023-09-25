1bf38aa303b94458b0999d9c6e9cdfa13d95b26f
Emanuele Torelli <emanueletorelli@outlook.com>
2023-09-25 12:32:48 +0200

    Remove id field from CMPermission

--- ClientModels/Runitime/Scripts/Models/CMPermission.cs
+++ ClientModels/Runitime/Scripts/Models/CMPermission.cs
@@ -7,12 +7,10 @@ namespace Reflectis.SDK.ClientModels
     [Serializable]
     public class CMPermission
     {
-        [SerializeField] private int id;
         [SerializeField] private CMFacet facet;
         [SerializeField] private CMTag tag;
         [SerializeField] private bool isEnabled;
 
-        public int Id { get => id; set => id = value; }
         public CMFacet Facet { get => facet; set => facet = value; }
         public bool IsEnabled { get => isEnabled; set => isEnabled = value; }
         public CMTag Tag { get => tag; set => tag = value; }
