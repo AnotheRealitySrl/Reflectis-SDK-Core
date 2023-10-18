c6d2bbf663ccb2aa1b6af0a21f87b9edcb88b27a
Riccardo Verza <feelthecabbage@gmail.com>
2023-10-18 19:22:29 +0200

    Added extension for strings to copy to clipboard.

---
+++ Utilities/Runtime/Scripts/Extensions/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Reflectis.SDK.Utilities.Extensions
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Puts the string into the Clipboard.
+        /// </summary>
+        public static void CopyToClipboard(this string str)
+        {
+            GUIUtility.systemCopyBuffer = str;
+        }
+    }
+}
