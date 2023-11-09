0ab841eaab46292b3a064ee7f0293e280f5dd129
Emanuele Torelli <emanueletorelli@outlook.com>
2023-11-09 17:03:31 +0100

    Add IInteractionSystem interface

---
+++ InteractionNew/Runtime/Scripts/IInteractionSystem.cs
@@ -0,0 +1,9 @@
+using Reflectis.SDK.Core;
+
+namespace Reflectis.SDK.InteractionNew
+{
+    public interface IInteractionSystem : ISystem
+    {
+        public bool IsHoveringUI { get; set; }
+    }
+}
