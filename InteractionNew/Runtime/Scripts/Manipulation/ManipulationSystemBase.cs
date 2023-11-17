0f7cb04ae3bd9805abe7ff1c389a568b9fc975cc
Elia Russo <rockrono_x@yahoo.it>
2023-11-17 16:33:45 +0100

    Made mew base Manipulation system class

---
+++ InteractionNew/Runtime/Scripts/Manipulation/ManipulationSystemBase.cs
@@ -0,0 +1,26 @@
+using Reflectis.SDK.Core;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Reflectis.SDK.InteractionNew
+{
+    public class ManipulationSystemBase : BaseSystem, IManipulationSystem
+    {
+        [SerializeField] protected GameObject scalablePointFacePrefab;
+        [SerializeField] protected Vector2 targetScaleFaces = new Vector3(0.15f, 0.15f, 0.15f);
+
+        [Header("Scriptable actions")]
+        [SerializeField] protected List<AwaitableScriptableAction> onHoverEnterActions = new();
+        [SerializeField] protected List<AwaitableScriptableAction> onHoverExitActions = new();
+
+        public GameObject ScalablePointFacePrefab => scalablePointFacePrefab;
+
+        public List<AwaitableScriptableAction> OnHoverEnterActions => onHoverEnterActions;
+        public List<AwaitableScriptableAction> OnHoverExitActions => onHoverExitActions;
+
+        public override void Init()
+        {
+
+        }
+    }
+}
