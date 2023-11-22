9c12a3ba35f511ed2c3d6510547e25f36badef9e
Elia Russo <rockrono@gmail.com>
2023-11-22 09:59:42 +0100

    Made new base version of model scaler behavior

---
+++ InteractionNew/Runtime/Scripts/Manipulation/ModelScaler.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Reflectis.SDK.InteractionNew
+{
+    public abstract class ModelScaler : MonoBehaviour
+    {
+
+        public abstract void ToggleNonProportionalGizmos(bool enable);
+    }
+}
