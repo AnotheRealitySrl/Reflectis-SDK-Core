5ee8db1a152603275914e1c48164acc72ae61ad8
Emanuele Torelli <emanueletorelli@outlook.com>
2024-01-02 23:03:38 +0100

    Add DictionaryExtensions

--- Utilities/Runtime/Scripts/Extensions/DictionaryExtensions.cs
+++ Utilities/Runtime/Scripts/Extensions/DictionaryExtensions.cs
@@ -9,5 +9,12 @@ namespace Reflectis.SDK.Utilities
         {
             return "{" + string.Join(",", dictionary.Select(kv => kv.Key + "=" + kv.Value).ToArray()) + "}";
         }
+
+        public static Dictionary<string, TValue> ToDictionary<TValue>(object obj)
+        {
+            var json = JsonConvert.SerializeObject(obj);
+            var dictionary = JsonConvert.DeserializeObject<Dictionary<string, TValue>>(json);
+            return dictionary;
+        }
     }
 }
