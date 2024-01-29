f91fc2aab634166f46c032cb73d52cece5f1a8ee
Emanuele Torelli <emanueletorelli@outlook.com>
2024-01-29 10:31:37 +0100

    Add Setup abstract method in model scaler

--- InteractionNew/Runtime/Scripts/Manipulation/ModelScaler.cs
+++ InteractionNew/Runtime/Scripts/Manipulation/ModelScaler.cs
@@ -4,6 +4,7 @@ namespace Reflectis.SDK.InteractionNew
 {
     public abstract class ModelScaler : MonoBehaviour
     {
+        public abstract void Setup();
         public abstract void ToggleNonProportionalGizmos(bool enable);
     }
 }
