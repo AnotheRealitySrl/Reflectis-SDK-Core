55ef20bc84c448242dc32f390f8a6023c0c21834
Emanuele Torelli <emanueletorelli@outlook.com>
2024-03-07 18:56:33 +0100

    Add catalog info to CMEnvironment

--- ClientModels/Runitime/Scripts/Models/CMEnvironment.cs
+++ ClientModels/Runitime/Scripts/Models/CMEnvironment.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+
 using UnityEngine;
 
 namespace Reflectis.SDK.ClientModels
@@ -13,6 +14,9 @@ namespace Reflectis.SDK.ClientModels
         [SerializeField] private string imageUrl;
         [SerializeField] private Texture imageTexture;
         [SerializeField] private string addressableKey;
+        [SerializeField] private string referenceCatalog;
+        [SerializeField] private string internalId;
+        [SerializeField] private string bundleId;
 
         public int ID { get => id; set => id = value; }
         public string Name { get => name; set => name = value; }
@@ -20,8 +24,11 @@ namespace Reflectis.SDK.ClientModels
         public string ImageUrl { get => imageUrl; set => imageUrl = value; }
         public Texture ImageTexture { get => imageTexture; set => imageTexture = value; }
         public string AddressableKey { get => addressableKey; set => addressableKey = value; }
+        public string ReferenceCatalog { get => referenceCatalog; set => referenceCatalog = value; }
+        public string InternalId { get => internalId; set => internalId = value; }
+        public string BundleId { get => bundleId; set => bundleId = value; }
 
-        
+
     }
 
     public class CMEnvironmentNameComparerer : IEqualityComparer<CMEnvironment>
