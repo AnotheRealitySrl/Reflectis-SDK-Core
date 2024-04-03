e9f27003613adc424b7eb79367c2cb0fa6f49dd7
Raffaele Tolusso <raffaele.tolusso@gmail.com>
2024-04-03 15:42:38 +0200

    Added scene hierarchy interface

---
+++ SceneHierarchy/Runtime/Scripts/ISceneHierarchySystem.cs
@@ -0,0 +1,27 @@
+using Reflectis.SDK.Core;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Reflectis.SDK.SceneHierarchy
+{
+    public interface ISceneHierarchySystem : ISystem
+    {
+        public void EnableOtherAvatars(bool enable, List<GameObject> except = null);
+
+        public void EnableSpawnedObjects(bool enable, List<GameObject> except = null);
+
+        public void EnableSpawnedNetworkObjects(bool enable, List<GameObject> except = null);
+
+        public void EnableSpawnedLocalObjects(bool enable, List<GameObject> except = null);
+
+        public void EnableEnvironment(bool enable, List<GameObject> except = null);
+
+        public void AddNetworkSpawnedObject(GameObject gameObject);
+
+        public void AddAvatar(GameObject avatar);
+
+        public void AddLocalSpawnedObject(GameObject localObject);
+
+        public void AddEnvironmentObject(GameObject environmentObject);
+    }
+}
