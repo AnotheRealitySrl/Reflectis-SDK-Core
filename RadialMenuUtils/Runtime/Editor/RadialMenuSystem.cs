7cdce55283d92a690f060fee6ba873825ed8023b
FrancescoWeik <fweikmann@gmail.com>
2024-04-18 16:52:19 +0200

    Add radialMenuSystem

---
+++ RadialMenuUtils/Runtime/Editor/RadialMenuSystem.cs
@@ -0,0 +1,32 @@
+using Reflectis.SDK.Core;
+using UnityEngine;
+
+namespace Reflectis.SDK.RadialMenuUtils
+{
+    [CreateAssetMenu(fileName = "New Radial Menu", menuName = "RadialMenu/Create New Radial Menu")]
+    public class RadialMenuSystem : BaseSystem
+    {
+        public GameObject radialMenuPrefab;
+        public GameObject radialMenuNetworkPrefab;
+
+        public GameObject instantiatedRadialMenu;
+
+        public GameObject InstantiateRadialMenu(bool network)
+        {
+            if (!network)
+            {
+                radialMenuPrefab.SetActive(false);
+                instantiatedRadialMenu = Instantiate(radialMenuPrefab);
+            }
+            else
+            {
+                radialMenuNetworkPrefab.SetActive(false);
+                instantiatedRadialMenu = Instantiate(radialMenuNetworkPrefab);
+                //instantiate radialRPCManager too... remember that it is a network element so maybe you have to instantiate it differently
+            }
+
+            Debug.LogError("RadialMenu instantiated, have fun!");
+            return instantiatedRadialMenu;
+        }
+    }
+}
