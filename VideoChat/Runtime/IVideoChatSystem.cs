234ffb2dee7ffdfee95dff10c4133e3e733175d9
Emanuele Torelli <emanueletorelli@outlook.com>
2024-07-22 12:55:46 +0200

    Add IVideoChatController, minor modifications to IVideoChatSystem

--- VideoChat/Runtime/IVideoChatSystem.cs
+++ VideoChat/Runtime/IVideoChatSystem.cs
@@ -1,15 +1,13 @@
 using Reflectis.SDK.Core;
 
-using UnityEngine;
-
 namespace Reflectis.SDK.VideoChat
 {
     public interface IVideoChatSystem : ISystem
     {
         string AppId { get; set; }
-        GameObject CurrentVideo { get; set; }
 
         void JoinChannel(string channelName);
         void LeaveChannel();
+        void CreateVideoView(IVideoChatController videoChatController, int userId);
     }
 }
