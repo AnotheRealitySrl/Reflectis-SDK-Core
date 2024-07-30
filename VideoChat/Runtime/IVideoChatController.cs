a6d1610c282c1f32533fb58aebd196e36dbf929c
Emanuele Torelli <emanueletorelli@outlook.com>
2024-07-30 20:12:36 +0200

    Add methods and events in video chat interfaces

--- VideoChat/Runtime/IVideoChatController.cs
+++ VideoChat/Runtime/IVideoChatController.cs
@@ -1,4 +1,13 @@
 public interface IVideoChatController
 {
+    public enum EVideoChatType
+    {
+        Webcam,
+        ScreenShare
+    }
+
+    uint UserId { get; }
+    public EVideoChatType VideoChatType { get; }
+    bool IsStreamer { get; }
     void Resize(float ratio);
 }
--- VideoChat/Runtime/IVideoChatSystem.cs
+++ VideoChat/Runtime/IVideoChatSystem.cs
@@ -1,10 +1,18 @@
 using Reflectis.SDK.Core;
 
+using UnityEngine.Events;
+
 namespace Reflectis.SDK.VideoChat
 {
     public interface IVideoChatSystem : ISystem
     {
         string AppId { get; set; }
+
+        UnityEvent<string, uint> OnChannelJoined { get; }
+        UnityEvent OnChannelLeft { get; }
+        UnityEvent<uint> OnUserJoined { get; }
+        UnityEvent<uint> OnUserLeft { get; }
+        UnityEvent<int, string> OnError { get; }
 
         void JoinChannel(string channelName);
         void LeaveChannel();
