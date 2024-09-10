54115f0623e98d50072b84e64d013592f8ed1946
Raffaele Tolusso <raffaele.tolusso@gmail.com>
2024-09-10 16:36:17 +0200

    Added IsNetworkMaster Property to reflectis application manager

--- ApplicationManagement/Runtime/Scripts/IReflectisApplicationManager.cs
+++ ApplicationManagement/Runtime/Scripts/IReflectisApplicationManager.cs
@@ -5,6 +5,8 @@ namespace Reflectis.SDK.ApplicationManagement
     public interface IReflectisApplicationManager : IApplicationManager
     {
         static new IReflectisApplicationManager Instance { get; protected set; }
+        //TO DO: Move this logic inside network system
+        bool IsNetworkMaster { get; }
 
         void InitializeObject(UnityEngine.Object reflectisObject);
     }
