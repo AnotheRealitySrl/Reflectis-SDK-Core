351c4c5e8485489b34ed3a34254f74668f1b5684
Raffaele Tolusso <raffaele.tolusso@gmail.com>
2024-10-09 17:21:04 +0200

    Fixed displayable data types

--- Diagnostics/Runtime/Scripts/DisplayableData/AnalyticsDisplayableData.cs
+++ Diagnostics/Runtime/Scripts/DisplayableData/AnalyticsDisplayableData.cs
@@ -2,13 +2,8 @@ namespace Reflectis.SDK.Diagnostics
 {
     public class AnalyticsDisplayableData
     {
-        public string type;
-        public string content;
+        public EAnalyticsDisplayableType type;
+        public DisplayableContentBase content;
 
-        public AnalyticsDisplayableData(string type, string content)
-        {
-            this.type = type;
-            this.content = content;
-        }
     }
 }
