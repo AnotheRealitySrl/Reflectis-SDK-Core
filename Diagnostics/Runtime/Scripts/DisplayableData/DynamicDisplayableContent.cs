4a3827ee6f3a8c1afe99c1a09722c71e3726dccc
Raffaele Tolusso <raffaele.tolusso@gmail.com>
2024-10-09 17:52:42 +0200

    Changed json serialization

--- Diagnostics/Runtime/Scripts/DisplayableData/DynamicDisplayableContent.cs
+++ Diagnostics/Runtime/Scripts/DisplayableData/DynamicDisplayableContent.cs
@@ -9,31 +9,28 @@ namespace Reflectis.SDK.Diagnostics
     public class DynamicDisplayableContent : DisplayableContentBase
     {
         [SettableField(isRequired = true)]
-        private List<string> headers;
+        public List<string> headers;
         [SettableField(isRequired = true)]
-        private List<string> types;
+        public List<string> types;
         /// <summary>
         /// Visual scripting does not handle correctly list of list of objects
         /// in type entry definitions, so we have to pass a list of object as 
         /// type definition
         /// </summary>
         [SettableField(isRequired = true, entryType = typeof(List<object>))]
-        private List<List<object>> records;
+        public List<List<object>> records;
 
-        public List<string> Headers { get => headers; set => headers = value; }
-        public List<string> Types { get => types; set => types = value; }
-        public List<List<object>> Records { get => records; set => records = value; }
 
         public override void CheckValidity()
         {
             int columnCount = headers.Count;
 
-            if (!(Types.Count == columnCount && (Records).All(x => ((List<object>)x).Count == columnCount)))
+            if (!(types.Count == columnCount && (records).All(x => ((List<object>)x).Count == columnCount)))
             {
                 throw new System.Exception($"Record types count are not matching.\n" +
                     $"Headers count: {headers.Count} \n" +
-                    $"Types count: {Types.Count} \n" +
-                    string.Join("", Records.Select((x, i) => $"Record {i}: {((List<object>)x).Count} \n")));
+                    $"Types count: {types.Count} \n" +
+                    string.Join("", records.Select((x, i) => $"Record {i}: {((List<object>)x).Count} \n")));
             }
         }
 
@@ -42,19 +39,19 @@ namespace Reflectis.SDK.Diagnostics
             var headerField = args.FirstOrDefault(x => x.name == "headers");
             if (headerField != null)
             {
-                Headers = headerField.value as List<string>;
+                headers = headerField.value as List<string>;
             }
             var typesField = args.FirstOrDefault(x => x.name == "types");
             if (typesField != null)
             {
-                Types = typesField.value as List<string>;
+                types = typesField.value as List<string>;
             }
             var recordField = args.FirstOrDefault(x => x.name == "records");
             if (recordField != null)
             {
                 List<object> recordsValue = recordField.value as List<object>;
 
-                Records = recordsValue.Select((x, i) =>
+                records = recordsValue.Select((x, i) =>
                 {
                     if (x is ArrayList aotList)
                     {
