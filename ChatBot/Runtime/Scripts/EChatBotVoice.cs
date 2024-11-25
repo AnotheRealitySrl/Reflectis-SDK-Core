3d30c88dc22586cbf38e1d4fb9e3087093e5d58d
Emanuele Torelli <emanueletorelli@outlook.com>
2024-11-25 09:16:06 +0100

    Add ChatBot and WebSocket modules

---
+++ ChatBot/Runtime/Scripts/EChatBotVoice.cs
@@ -0,0 +1,18 @@
+namespace Reflectis.SDK.ChatBot
+{
+    public enum EChatBotVoice
+    {
+        alloy,
+        echo,
+        shimmer,
+        amuch,
+        dan,
+        elan,
+        marilyn,
+        meadow,
+        breeze,
+        cove,
+        ember,
+        jupiter
+    }
+}
