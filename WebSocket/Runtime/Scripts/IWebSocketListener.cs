06a26e8f18be4f556c50e8ba47dcb89d5655681f
Raffaele Tolusso <raffaele.tolusso@gmail.com>
2024-12-24 18:03:55 +0100

    added opening error

--- WebSocket/Runtime/Scripts/IWebSocketListener.cs
+++ WebSocket/Runtime/Scripts/IWebSocketListener.cs
@@ -7,6 +7,7 @@ namespace Reflectis.SDK.WebSocket
     public interface IWebSocketListener
     {
         public void OnWebSocketOpen();
+        public void OnWebSocketOpeningError(string error);
         public void OnWebSocketError(string error);
         public void OnWebSocketMessageReceived(string data);
         public void OnWebSocketClose();
