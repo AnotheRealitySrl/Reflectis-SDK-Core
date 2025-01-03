af8cbd15ade2136384526d352e0b74599ed4ebc1
Raffaele Tolusso <raffaele.tolusso@gmail.com>
2025-01-03 12:07:46 +0100

    Improved webSocket system

--- WebSocket/Runtime/Scripts/IWebSocketListener.cs
+++ WebSocket/Runtime/Scripts/IWebSocketListener.cs
@@ -6,8 +6,6 @@ namespace Reflectis.SDK.WebSocket
 {
     public interface IWebSocketListener
     {
-        public void OnWebSocketOpen();
-        public void OnWebSocketOpeningError(string error);
         public void OnWebSocketError(string error);
         public void OnWebSocketMessageReceived(string data);
         public void OnWebSocketClose();
--- WebSocket/Runtime/Scripts/IWebSocketSystem.cs
+++ WebSocket/Runtime/Scripts/IWebSocketSystem.cs
@@ -1,5 +1,5 @@
 using Reflectis.SDK.Core;
-
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -9,8 +9,44 @@ namespace Reflectis.SDK.WebSocket
 {
     public interface IWebSocketSystem : ISystem
     {
-        Task ConnectAsync(string url, Dictionary<string, string> queryParams, IWebSocketListener webSocketListener);
+        /// <summary>
+        /// Connect to a webSocket
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="queryParams"></param>
+        /// <param name="onWebSocketOpen"></param>
+        /// <param name="onWebSocketOpenError">if there was an error while opening the websocket call the action with the error message param</param>
+        /// <returns></returns>
+        Task ConnectAsync(string url, Dictionary<string, string> queryParams, Action onWebSocketOpen = null, Action<string> onWebSocketOpenError = null);
+
+        /// <summary>
+        /// Add listener to the webSocket
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="webSocketListener"></param>
+        void AddListener(string url, IWebSocketListener webSocketListener);
+
+        /// <summary>
+        /// Disconnect this listener from the socket, this will NOT call the listener onClose
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="webSocketListener"></param>
+        /// <returns></returns>
+        void RemoveListener(string url, IWebSocketListener webSocketListener);
+
+        /// <summary>
+        /// Hard disconnect from the socket
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
         Task DisconnectAsync(string url);
+
+        /// <summary>
+        /// Send message to a websocket we are connected to. 
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="message"></param>
+        /// <returns></returns>
         public Task SendMessageAsync(string url, string message);
 
     }
