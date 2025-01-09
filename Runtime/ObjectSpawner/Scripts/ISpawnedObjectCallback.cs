29a01dc286c0c365ce5885917f4695e4ce8a2fa2
Emanuele Torelli <emanueletorelli@outlook.com>
2025-01-09 14:24:53 +0100

    SDK core refactor WIP

--- Runtime/ObjectSpawner/Scripts/ISpawnedObjectCallback.cs
+++
@@ -1,7 +0,0 @@
-namespace Reflectis.ObjectSpawner
-{
-    public interface ISpawnedObjectCallback
-    {
-        public void OnObjectSpawned(object[] instantiationData);
-    }
-}
--- Runtime/ObjectSpawner/Scripts/SpawnData.cs
+++
@@ -1,8 +0,0 @@
-
-namespace Reflectis.ObjectSpawner
-{
-    public abstract class SpawnData
-    {
-        public abstract string Key { get; }
-    }
-}
