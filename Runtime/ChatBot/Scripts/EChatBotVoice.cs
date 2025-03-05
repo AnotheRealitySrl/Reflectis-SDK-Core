96c28e7594359596ff7870ea98b5d1c60fa7d9b2
Raffaele Tolusso <raffaele.tolusso@gmail.com>
2025-03-05 14:47:56 +0100

    Fix chatbot voices

--- Runtime/ChatBot/Scripts/EChatBotVoice.cs
+++ Runtime/ChatBot/Scripts/EChatBotVoice.cs
@@ -2,17 +2,13 @@ namespace Reflectis.SDK.Core.ChatBot
 {
     public enum EChatBotVoice
     {
-        alloy,
-        echo,
-        shimmer,
-        amuch,
-        dan,
-        elan,
-        marilyn,
-        meadow,
-        breeze,
-        cove,
-        ember,
-        jupiter
+        Alloy,
+        Ash,
+        Ballad,
+        Coral,
+        Echo,
+        Sage,
+        Shimmer,
+        Verse
     }
 }
